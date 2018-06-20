050ebd28e0a6587f8f15f08dd9fa09eb4c72c3a2
jlamberti <jlamberti2015@gmail.com>
2018-06-20 00:24:00 -0600

    added login

---
+++ client/src/login.js
@@ -0,0 +1,43 @@
+import React from 'react';
+// import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import axios from 'axios';
+
+export class Login extends React.Component {
+    async handleSubmit(e) {
+        const password = document.getElementById('password').value;
+        const self = this;
+        e.preventDefault();
+        await axios.post('/login', {
+            password: password
+        })
+            .then(function (response) {
+                if (response.data.msg === 'success') {
+                    alert("Logged In");
+                    self.resetForm();
+                } else if (response.data.msg === 'fail') {
+                    alert("Log In Failed")
+                }
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+    static resetForm () {
+        document.getElementById('contact-form').reset();
+    }
+
+    render () {
+        return (
+            <div className="contact">
+                <form id="contact-form" onSubmit={this.handleSubmit.bind(this)} method="POST">
+                    <div className="form-group">
+                        <input type="text" className="form-control" id="password" autoComplete="off" />
+                    </div>
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        )
+    }
+}
+                        // <label htmlFor="exampleInputEmail1">Email</label>
