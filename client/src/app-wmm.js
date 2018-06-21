da96e8ca7420f60155860630c3a7b1d40621e725
jlamberti <jlamberti2015>
2018-06-21 12:35:56 -0600

    Added new backgrounds

--- client/src/app-wmm.js
+++ client/src/app-wmm.js
@@ -3,6 +3,9 @@ import axios from 'axios';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Feed } from "./feed";
 import logo from './img/wmm.png';
+import logo2 from './img/logo2.png';
+import logo3 from './img/logo3.png';
+import logoGif from './img/logo.gif';
 import { Shop } from "./shop";
 import { Header } from "./header";
 import { Contact } from "./contact";
@@ -10,7 +13,7 @@ import { SmokeFree } from "./smoke-free";
 import { Subscribe } from "./subscribe";
 import { Login } from "./login";
 
-const Intro = () => <a href="/feed"><img src={logo} alt="WMM" className="logo"/></a>;
+const Intro = () => <a href="/feed"><img src={logo3} alt="WMM" className="logo"/></a>;
 const Error = () => <h1>404..</h1>;
 
 export class WMM extends React.Component {
