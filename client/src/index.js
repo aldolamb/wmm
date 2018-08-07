7d44f5cee93175b1f87973f0355d675287aa5870
jlamberti <jlamberti2015@gmail.com>
2018-08-07 12:40:17 -0600

    total rewrite and deploy

--- client/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { WMM } from './app-wmm';
-import './css/main.css';
-
-ReactDOM.render(<WMM />, document.getElementById('root'));
