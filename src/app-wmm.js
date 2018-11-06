4bc05755be1e342c4bfa619a73458ae8789fa9ea
jlamberti <jlamberti2015@gmail.com>
2018-11-06 13:27:09 -0700

    some other initial git but fixes

--- src/app-wmm.js
+++ src/app-wmm.js
@@ -5,52 +5,17 @@ import { Header } from "./header";
 import { Footer } from "./footer";
 import { Main } from "./main";
 
-// import firebase from "firebase";
-// const config = {
-//     apiKey: "AIzaSyDNJbRL7IkdkxZ1bxc2faMH3AMmK0H0cvk",
-//     authDomain: "wmmdata-42f0b.firebaseapp.com",
-//     databaseURL: "https://wmmdata-42f0b.firebaseio.com",
-//     projectId: "wmmdata-42f0b",
-//     storageBucket: "wmmdata-42f0b.appspot.com",
-//     messagingSenderId: "16374020327"
-// };
-// firebase.initializeApp(config);
-
-
 export class WMM extends React.Component {
     render() {
         return (
             <div className="main">
                 <Header/>
-                <Router>
-                    {/*{sessionStorage.getItem("loggedIn") ?*/}
-                    {/*<Switch>*/}
-                        {/*<Route path='/feed/edit/:postID'    component={Edit} />*/}
-                        {/*<Route path='/feed/:postID'         component={Post} />*/}
-                        {/*<Route path='/feed'                 component={Feed} />*/}
-                        {/*<Route path='/shop'                 component={Shop} />*/}
-                        {/*<Route path='/contact'              component={Contact} />*/}
-                        {/*<Route path='/upload'               component={Upload} />*/}
-                        {/*<Route path='/login'                component={Login} />*/}
-                        {/*<Route exact path='/'               component={Main} />*/}
-                        {/*<Route path='*'                     component={Error} />*/}
-                    {/*</Switch> :*/}
-                    {/*<Switch>*/}
-                        {/*<Route path='/upload'               component={Upload} />*/}
-                        {/*<Route path='/feed/:postID'         component={Post} />*/}
-                        {/*<Route path='/feed'                 component={Feed} />*/}
-                        {/*<Route path='/zines'                component={Zines} />*/}
-                        {/*<Route path='/contact'              component={Contact} />*/}
-                        {/*<Route path='/login'                component={Login} />*/}
-                        {/*<Route exact path='/'               component={Main} />*/}
-                        {/*<Route path='*'                     component={Error} />*/}
-                    {/*</Switch>*/}
-                    {/*}*/}
-                    <Switch>
-                        <Route path='/zines'                component={Zines} />
-                        <Route exact path='/'               component={Main} />
-                    </Switch>
-                </Router>
+                    <Router>
+                        <Switch>
+                            <Route path='/zines'                component={Zines} />
+                            <Route exact path='/'               component={Main} />
+                        </Switch>
+                    </Router>
                 <Footer/>
             </div>
         )
